f9b721ac95bb8639f9626427d64eb8d9cd82beb1
Antonio Hernandez <jose.hernandezam@elektra.com.mx>
2023-01-04 14:50:00 -0600

    Initial Commit

---
+++ SimpleMathApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SimpleMathApp
+//
+//  Created by Jose Antonio Hernandez on 04/01/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
