c3dcb58b1d8e77462a69daf696207e38e4eb1c5c
JOSE ANTONIO HERNANDEZ AMBROCIO <jose.hernandezam@elektra.com.mx>
2023-01-09 15:55:53 -0600

    adds simpleMathPackage

--- SimpleMathApp/ViewController.swift
+++ SimpleMathApp/ViewController.swift
@@ -6,14 +6,15 @@
 //
 
 import UIKit
+import SimpleMathFramework
 
 class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        print(SimpleAlgorithms.factorial(n: 5))
     }
-
 
 }
 
